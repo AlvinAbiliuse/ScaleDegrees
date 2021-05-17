24f1d98d312053272f8b5003b0861b183024ec88
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-05-17 12:37:45 +0400

    scaleDegrees.py added logging

--- scaleDegrees.py
+++ scaleDegrees.py
@@ -1,5 +1,7 @@
 from random import randint
 from time import sleep
+from sys import exc_info
+from datetime import datetime
 
 def quiz(scaleDict):
 	for i in range(25):
@@ -9,4 +11,10 @@ def quiz(scaleDict):
 
 GmajorScale = {'1':'G', '2':'A', '3':'B', '4':'C', '5':'D', '6':'E',
 				 '7':'F#'}
-quiz(GmajorScale)
+
+if __name__ == "__main__":
+	try:
+		quiz(GmajorScale)
+	except:
+		with open('log.txt', 'a+') as log:
+			log.write('Logging date:' + str(datetime.now().strftime("%d-%b-%Y %H:%M")) + '\n\n' + str(exc_info()[0]) + '\n\n\n')
