cf52085c3253dac4ff115830c9473de7e0944837
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-05-17 12:15:44 +0400

    scaleDegrees.py added sleep between prints

--- scaleDegrees.py
+++ scaleDegrees.py
@@ -4,6 +4,7 @@ from time import sleep
 def quiz(scaleDict):
 	for i in range(25):
 		print(f'Play {scaleDict[str(randint(1, 8))]}')
+		sleep(2)
 
 
 GmajorScale = {'1':'G', '2':'A', '3':'B', '4':'C', '5':'D', '6':'E',
