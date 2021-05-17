39f60eecce0492c5dcf61acf9caedaead96aa611
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-05-17 12:20:49 +0400

    remove 8th degree

--- scaleDegrees.py
+++ scaleDegrees.py
@@ -3,10 +3,10 @@ from time import sleep
 
 def quiz(scaleDict):
 	for i in range(25):
-		print(f'Play {scaleDict[str(randint(1, 8))]}')
+		print(f'Play {scaleDict[str(randint(1, 7))]}')
 		sleep(2)
 
 
 GmajorScale = {'1':'G', '2':'A', '3':'B', '4':'C', '5':'D', '6':'E',
-				 '7':'F#', '8':'G'}
+				 '7':'F#'}
 quiz(GmajorScale)
