36d024b5296f049a35c226116982f1a34a32f99f
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-05-17 12:14:27 +0400

    scaleDegrees.py changed int to str

--- scaleDegrees.py
+++ scaleDegrees.py
@@ -3,9 +3,9 @@ from time import sleep
 
 def quiz(scaleDict):
 	for i in range(25):
-		print('Play {scaleDict[randint(1, 8)]}')
+		print(f'Play {scaleDict[str(randint(1, 8))]}')
 
 
 GmajorScale = {'1':'G', '2':'A', '3':'B', '4':'C', '5':'D', '6':'E',
-				 '7':'F#'}
+				 '7':'F#', '8':'G'}
 quiz(GmajorScale)
