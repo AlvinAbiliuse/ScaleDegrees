37cbd368ec3000fb5f464fc329d8a018c0b43bb9
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-05-20 07:36:07 +0400

    scaleDegrees.py progress

--- scaleDegrees.py
+++ scaleDegrees.py
@@ -1,20 +1,40 @@
-from random import randint
+#!/usr/bin/env python3
+
+
+import random
 from time import sleep
 from sys import exc_info
 from datetime import datetime
 
-def quiz(scaleDict):
-	for i in range(25):
-		print(f'Play {scaleDict[str(randint(1, 7))]}')
-		sleep(2)
+def quiz(key, degrees):
+	while True:
+		random.shuffle(keys)
+		print(f'Key is {keys[0]}')
+		for i in range(25):
+			print(f'Play {random.shuffle(degrees)[0]}')
+			sleep(2)
+		print('Key Change!')
 
+keys = ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'Gb', 'Db', 'C#', 'Ab', 'Eb', 'Bb', 'F']
 
-GmajorScale = {'1':'G', '2':'A', '3':'B', '4':'C', '5':'D', '6':'E',
-				 '7':'F#'}
+majorScaleDegrees = [1, 2, 3, 4, 5, 6, 7]
+majorScaleThirds = [1, 3, 5, 7]
+minorScaleDegrees = list()
+
 
 if __name__ == "__main__":
-	try:
-		quiz(GmajorScale)
-	except:
-		with open('log.txt', 'a+') as log:
-			log.write('Logging date:' + str(datetime.now().strftime("%d-%b-%Y %H:%M")) + '\n\n' + str(exc_info()[0]) + '\n\n\n')
+#	try:
+	input1 = input('Major or Minor!:   ').lower()
+	input2 = input('Thirds or all!:   ').lower()
+	print(input1 + '  ' + input2)
+	if input1 == "major":
+		if input2 == "thirds":
+			quiz(keys, majorScaleThirds)
+		elif input2 == "all":
+			quiz(keys, majorScaleDegrees)
+	elif input1 == "minor":
+		print('', end='')
+		
+#	except:
+#		with open('log.txt', 'a+') as log:
+#			log.write('Logging date:' + str(datetime.now().strftime("%d-%b-%Y %H:%M")) + '\n\n' + str(exc_info()[0]) + '\n\n\n')
