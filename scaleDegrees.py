cb840b9fdaf396d2af2d85ec7e37adccb6dc28de
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-07-12 12:33:39 +0400

    scaleDegrees.py added custom timer

--- scaleDegrees.py
+++ scaleDegrees.py
@@ -33,25 +33,25 @@ majorScaleThirds = ['1', '3', '5', '7']
 
 # Chooses a random key and displays scale degrees randomly after a
 # sleep period for 25 turns and then switches keys. loops until ctrl+c
-def quiz(key, degrees):
+def quiz(key, degrees, timer):
 	while True:
 		shuffle(keys)
 		print(f'Key is {keys[0]}')
 		for i in range(25):
 			shuffle(degrees)
 			print(f'Play {degrees[0]}')
-			sleep(2)
+			sleep(timer)
 		print('Key Change!')
 
 
 # Displays random key every 10 seconds. to be used as a tool to memorize
 # all the notes in the key.
-def randomNotes():
+def randomNotes(timer):
 	while True:
 		shuffle(keys)
 		for i in keys:
 			print(i)
-			sleep(10)
+			sleep(timer)
 
 
 if __name__ == "__main__":
@@ -60,18 +60,26 @@ if __name__ == "__main__":
 		try:
 			choice = int(input('Enter the Number of your choice: \n\
     1. Major Triads\n    2. All Major Notes\n    3. Minor Triads\n\
-    4. All Minor Notes\n    5. Root Notes Only'))
+    4. All Minor Notes\n    5. Root Notes Only\n'))
+			break
+		except ValueError:
+			print('You need to input a number')
+	# input for the sleep duration between notes
+	while True:
+		try:
+			timerChoice = int(input('Enter the time for sleep in \
+seconds: '))
 			break
 		except ValueError:
 			print('You need to input a number')
 	# if elif statements to call the correct  function with the correct
 	# parameters
 	if choice == 1:
-		quiz(keys, majorScaleThirds)
+		quiz(keys, majorScaleThirds, timerChoice)
 	elif choice == 2:
-		quiz(keys, majorScaleDegrees)
+		quiz(keys, majorScaleDegrees, timerChoice)
 	elif choice == 5:
-		randomNotes()
+		randomNotes(timerChoice)
 	# minor scale thirds to be added
 	# minor scale degrees to be added
 
