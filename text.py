2245b203d1d3cf31533a7deb9e5e9ee87b65308c
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-08-25 00:10:48 +0400

    text2speech test

---
+++ text.py
@@ -0,0 +1,12 @@
+from gtts import gTTS
+import os
+
+mytext = "Hello World!"
+
+myobj = gTTS(text=mytext, lang='en', slow=False)
+
+myobj.save("welcome.mp3")
+# while True:
+while True:
+	os.system("cvlc ./welcome.mp3 vlc://quit")
+	print('Hello World!')
